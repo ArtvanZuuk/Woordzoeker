df4f3d07887a54f72779ecb63bb35d0f0280af4b
Neville <147263@helenparkhurst.net>
2016-01-21 21:30:41 +0100

    output bijna afgemaakt en uitleg cq commentaar toegevoegd

--- Output.php
+++ Output.php
@@ -1,45 +1,49 @@
 <?php
-    function build_table($array){
-    // start table
+//functie om een tabel te kunnen maken
+function build_table($array){
     $html = '<table>';
-
-    // data rows
     foreach( $array as $key=>$value){
-        //var_dump($value);
         $html .= '<tr>';
         foreach($value as $key2=>$value2){
             $html .= '<td>' . $value2 . '</td>';
         }
         $html .= '</tr>';
     }
-
-    // finish table and return it
-
     $html .= '</table>';
     return $html;
 }
 
+//array met letters om minnetjes te veranderen
 $alfabet = array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", 
     "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
 );
 
+//woordzoeker.txt in een array zetten per regel
 $woordenzoeker = file("woordzoeker.txt");
 
+//array splitsen in een woordzoeker deel en een zoekwoorden deel
+$witregel = file("witregel.txt");
+$witregel = $witregel[0];
+$MagischWitRegelNummer = array_search($witregel, $woordenzoeker);
+unset($woordenzoeker[$MagischWitRegelNummer]);
+$zoekwoorden = array_slice($woordenzoeker, $MagischWitRegelNummer);
+$woordenzoeker = array_slice($woordenzoeker,0 ,$MagischWitRegelNummer);
+
+//woordzoeker array met regels verder splitsen in een multidimensionale array met lose letters
 foreach ($woordenzoeker as &$regel){
-    $regel = str_split($regel);
+    $regel = str_split(trim($regel));
 }
 
+//minnetjes veranderen in plusjes
 foreach ($woordenzoeker as &$value3) {
-    $elementToBeDeleted = sizeof($value3) - 1;
-    unset($value3[$elementToBeDeleted]);
     foreach ($value3 as &$value4){
-        if($value4 == "-" or $value4 == "/n"){
+        if($value4 == "-"){
             $value4 = $alfabet[rand(0, 25)];
         }
     }
 }
-
 ?>
+
 
 <html>
     <head>
@@ -51,9 +55,19 @@ foreach ($woordenzoeker as &$value3) {
     <body>
         <div id="tabel">
             <?php
+                //de tabel laten zien
                 echo build_table($woordenzoeker);
-                echo $woordenzoeker[0][0];
+            ?>   
+            <div id="zoekwoorden">
+                </br>
+                <?php
+                //het lijstje met zoekwoorden laten zien
+                foreach ($zoekwoorden as $zoekwoord) {
+                    echo "$zoekwoord</br>";
+                }
+                unset($zoekwoord);
             ?>
+            </div>
         </div>
     </body>
 </html>
