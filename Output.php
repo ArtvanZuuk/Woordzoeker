80067a2e645a511e299be4a855ecac116e8a9867
Neville <147263@helenparkhurst.net>
2016-01-30 12:41:49 +0100

    geen idee wat dit is.. maar het stond er al voordat ik begon

--- Output.php
+++ Output.php
@@ -1,29 +1,28 @@
 <?php
-
 //functie om een tabel te kunnen maken van een array
 
 function build_table($array) {
-    $html = '<table>';
-    $xcordinaat = "0";
-    $ycordinaat = "0";
-    foreach ($array as $key => $value) {
-        $html .= '<tr>';
-        foreach ($value as $key2 => $value2) {
-            $cordinaat = $xcordinaat . "," . $ycordinaat;
-            $html .= "<td class='$cordinaat'>" . $value2 . '</td>';
-            $xcordinaat = $xcordinaat + 1;
-        }
-        $html .= '</tr>';
-        $ycordinaat = $ycordinaat + 1;
-        $xcordinaat = "0";
-    }
-    $html .= '</table>';
-    return $html;
+$html = '<table>';
+$xcordinaat = "0";
+$ycordinaat = "0";
+foreach ($array as $key => $value) {
+$html .= '<tr>';
+foreach ($value as $key2 => $value2) {
+$cordinaat = $xcordinaat . "," . $ycordinaat;
+$html .= "<td class='$cordinaat'>" . $value2 . '</td>';
+$xcordinaat = $xcordinaat + 1;
+}
+$html .= '</tr>';
+$ycordinaat = $ycordinaat + 1;
+$xcordinaat = "0";
+}
+$html .= '</table>';
+return $html;
 }
 
 //array met letters om minnetjes te veranderen
 $alfabet = array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-    "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+ "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
 );
 
 //woordzoeker.txt in een array zetten per regel
@@ -36,7 +35,7 @@ $MagischWitRegelNummer = array_search($witregel, $woordenzoeker);
 unset($woordenzoeker[$MagischWitRegelNummer]);
 $zoekwoorden = array_slice($woordenzoeker, $MagischWitRegelNummer);
 foreach ($zoekwoorden as &$zoekwoord) {
-    $zoekwoord = trim($zoekwoord);
+$zoekwoord = trim($zoekwoord);
 }
 $woordenzoeker = array_slice($woordenzoeker, 0, $MagischWitRegelNummer);
 
@@ -44,39 +43,43 @@ $woordenzoeker = array_slice($woordenzoeker, 0, $MagischWitRegelNummer);
 
 //woordzoeker array met regels verder splitsen in een multidimensionale array met lose letters
 foreach ($woordenzoeker as &$regel) {
-    $regel = str_split(trim($regel));
+$regel = str_split(trim($regel));
 }
 
 //minnetjes veranderen in plusjes
 foreach ($woordenzoeker as &$value3) {
-    foreach ($value3 as &$value4) {
-        if ($value4 == "-") {
-            $value4 = $alfabet[rand(0, 25)];
-        }
-    }
+foreach ($value3 as &$value4) {
+if ($value4 == "-") {
+$value4 = $alfabet[rand(0, 25)];
+}
+}
 }
 
 //letters in een regel tellen
-$regelletters  = count($woordenzoeker[0]);
-echo $regelletters;
-
+$regelletters = count($woordenzoeker[0]);
+//echo $regelletters;
 //zoekwoorden ontdoen van hoofdletters zodat ze gezocht kunnen worden
 //gzw = gesplitst zoekwoord
 //gesplitst = gesplitste zoekwoorden
 $zoekwoorden = array_map('strtolower', $zoekwoorden);
 $gesplitst = $zoekwoorden;
 foreach ($gesplitst as &$gzw) {
-    $gzw = str_split($gzw);
+$gzw = str_split($gzw);
 }
-echo "<pre>", print_r($gesplitst, true), "</pre>";
-
-foreach ($gesplitst as $zoekendwoord) {
-    $aantalletters = count($zoekendwoord);
-    for ($i = 1; $i <= $aantalletters; $i++) {
-        if ($zoekendwoord) {
-            
+//echo "<pre>", print_r($gesplitst, true), "</pre>";
+foreach ($woordenzoeker as $woordenzoekerregel) {
+    foreach ($gesplitst as $zoekendwoord) {
+        $aantalkeer = $regelletters - count($zoekendwoord);
+        $check = 0;
+        for ($i = 0; $i <= $aantalkeer; $i++) {
+            if ($zoekendwoord[$i] === $woordenzoekerregel[$i]) {
+                $check = $check + 1;
+            }
         }
-    }
+    if ($check = $count($zoekendwoord)){
+
+}
+}
 }
 ?>
 
